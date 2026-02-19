030fb35f27f32c8cf8b03a9d8dc70945c05744ca
Bernardo Varela <berasb20@gmail.com>
2026-02-18 21:17:48 -0500

    Primer commit - Sala Tour 360

---
+++ project-title/data.js
@@ -0,0 +1,219 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-s1",
+      "name": "S1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8933194266724804,
+          "pitch": 0.6350208415876644,
+          "rotation": 0,
+          "target": "1-s2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-s2",
+      "name": "S2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6070225454525158,
+          "pitch": 0.6709912665592555,
+          "rotation": 0,
+          "target": "0-s1"
+        },
+        {
+          "yaw": -0.2155993986132092,
+          "pitch": 0.5251117601829716,
+          "rotation": 0,
+          "target": "2-s3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-s3",
+      "name": "S3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4396884813345023,
+          "pitch": 0.7295302660076146,
+          "rotation": 0,
+          "target": "3-s4"
+        },
+        {
+          "yaw": -1.9491959391484812,
+          "pitch": 0.8372641305400848,
+          "rotation": 0,
+          "target": "1-s2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-s4",
+      "name": "S4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7255122597273873,
+          "pitch": 0.6494324829816982,
+          "rotation": 0,
+          "target": "4-s5"
+        },
+        {
+          "yaw": 0.0501103813835293,
+          "pitch": 0.752523044306578,
+          "rotation": 0,
+          "target": "2-s3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-s5",
+      "name": "S5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1164373421612943,
+          "pitch": 0.7577056927026504,
+          "rotation": 0,
+          "target": "3-s4"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
